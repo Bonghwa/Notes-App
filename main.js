d1442b2adc9204034f569d4ca7ab40e1e2e018a3
Bonghwa <88541301+Bonghwa@users.noreply.github.com>
2022-02-24 23:32:20 +0900

    JS: Show notes

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const noteBtn = document.querySelector('#add-btn'),
 
 function getNotes() {
   let notes = localStorage.getItem('notes');
-  if ((notes = null)) {
+  if (notes == null) {
     notesObj = [];
   } else {
     notesObj = JSON.parse(notes);
@@ -30,4 +30,36 @@ noteBtn.addEventListener('click', (e) => {
   localStorage.setItem('notes', JSON.stringify(notesObj));
 
   form.reset();
+  showNotes();
 });
+
+function showNotes() {
+  getNotes();
+
+  let html = '';
+  notesObj.forEach((element, index) => {
+    html += `
+    <div class="note">
+        <div class="note-cta">
+            <p class="note-counter">Note ${index + 1}</p>
+            <div class="note-cta-btn">
+                <button class="note-btn" id="${index}"><i class="fas fa-trash"></i>Delete</button>
+                <button class="note-btn edit-btn" id="${index}"><i class="fas fa-edit"></i>Edit</button>
+            </div>
+        </div>
+        <hr />
+        <h3 class="note-title">Title: ${element.title}</h3>
+        <p class="note-text">${element.text}</p>
+    </div>
+    `;
+  });
+
+  const noteElm = document.querySelector('#notes');
+  if (notesObj.length != 0) {
+    noteElm.innerHTML = html;
+  } else {
+    noteElm.innerHTML = `<p>No notes added. Please add a note.</p>`;
+  }
+}
+
+showNotes();
